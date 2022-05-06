ef577d1bf5bb0d3ef9e52cdf31c4378162c26e3a
manigoldTLC <gabrxavier353@gmail.com>
2022-05-05 21:15:04 -0300

    alterado a forma de montagem dos cards e get agora através do fetch

--- js/get-alunos.js
+++ js/get-alunos.js
@@ -1,22 +1,10 @@
+const url = "https://five-turma-3.herokuapp.com/alunos";
+
 window.onload = function () {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("GET", "https://five-turma-3.herokuapp.com/alunos");
-
-    xhr.addEventListener("load", function () {
-
-        if (xhr.status == 200) {
-
-            let resposta = xhr.responseText;
-            let alunos = JSON.parse(resposta);
-
-            alunos.lista.forEach(aluno => {
-                adicionaCardSecao(aluno);
-            })
-
-        }
-
-    })
-
-    xhr.send();
+    fetch(url)
+        .then(resposta => resposta.json())
+        .then(data => {
+            getAlunos(data.lista)
+        })
+    
 }
--- js/monta-card.js
+++ js/monta-card.js
@@ -1,105 +1,41 @@
-function addNome(dado) {
-    let nome = document.createElement("span");
-    nome.classList.add("card__name");
-    nome.textContent = dado;
+const listaAlunos = document.getElementById("lista-alunos");
+let card = ''
 
-    return nome;
-}
+function getAlunos(alunos) {
+    alunos.forEach(aluno => {
 
-function addAniversario(dado) {
+        card += `
+            <article class="card col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card__top top">
+                    <figure class="card__imgcontainer">
+                        <img class="card__img" src="assets/img/manigold.png" alt="imagem usuário">
+                    </figure>
 
-    let aniversario = document.createElement("span");
-    aniversario.classList.add("card__birthdate");
-    aniversario.textContent = dado;
+                    <div class="card__texts">
+                        <span class="card__name">${nullValue(aluno.nm_pessoa, "Nome não informado")}</span>
+                        <span class="card__birthdate">${nullValue(aluno.dt_nascimento, "Data de nascimento não informado")}</span>
+                    </div>
+                </div>
 
-    return aniversario;
-}
+                <div class="card__middle middle">
+                    <span class="card__obs-title">Observações</span>
+                    <span class="card__obs">${nullValue(aluno.ds_observacao, "Nenhuma observacao informada")}</span>
+                </div>
 
-function addObservacao(dado) {
-    let observacao = document.createElement("span");
-    observacao.classList.add("card__obs");
-    observacao.textContent = dado;
-
-    return observacao;
-}
-
-
-function cardAluno(aluno) {
-    let card = document.createElement("article");
-    const listaClassesCard = ["card", "col-12", "col-md-6", "col-lg-4", "col-xl-3"]
-
-    listaClassesCard.forEach(classe => {
-        card.classList.add(classe)
-    })
-
-    let cardTop = document.createElement("div");
-    cardTop.classList.add("card__top");
-    cardTop.classList.add("top");
-
-    let cardMiddle = document.createElement("div");
-    cardMiddle.classList.add("card__middle");
-    cardMiddle.classList.add("middle");
-
-    let cardBottom = document.createElement("div");
-    cardBottom.classList.add("card__bottom");
-
-    let linkCard = document.createElement("a");
-    linkCard.classList.add("card__a");
-    linkCard.href = "#";
-    linkCard.textContent = "editar";
-
-
-    linkCard.setAttribute("idAluno", aluno.nr_sequencia);
-
-    linkCard.addEventListener("click", (e) => {
-        const id = e.target.getAttribute("idAluno");
-
-        editarAluno(id);
+                <div class="card__bottom">
+                    <a class="card__a" href="edit-form.html">editar</a>
+                </div>
+            </article>
+        `;
     });
 
-
-    cardBottom.appendChild(linkCard);
-
-    let middleTitle = document.createElement("span");
-    middleTitle.classList.add("card__obs-title");
-    middleTitle.textContent = "Observações";
-
-    cardMiddle.appendChild(middleTitle);
-    cardMiddle.appendChild(addObservacao(aluno.ds_observacao));
-
-    let figureCard = document.createElement("figure");
-    figureCard.classList.add("card__imgcontainer");
-
-    let imagemCard = document.createElement("img");
-    imagemCard.classList.add("card__img");
-    imagemCard.src = "assets/img/manigold.png";
-    imagemCard.alt = "imagem usuário";
-
-    figureCard.appendChild(imagemCard);
-
-    let cardTexts = document.createElement("div");
-    cardTexts.classList.add("card__texts");
-
-    cardTexts.appendChild(addNome(aluno.nm_pessoa));
-    cardTexts.appendChild(addAniversario(aluno.dt_nascimento));
-
-    cardTop.appendChild(figureCard);
-    cardTop.appendChild(cardTexts);
-
-    card.appendChild(cardTop);
-    card.appendChild(cardMiddle);
-    card.appendChild(cardBottom);
-
-
-    return card;
+    listaAlunos.innerHTML = card;
 }
 
-function retornaID(id) {
-    return id
-}
-
-function adicionaCardSecao(aluno) {
-    let alunoCard = cardAluno(aluno);
-    let secao = document.querySelector("#secao");
-    secao.appendChild(alunoCard)
+function nullValue(aluno, mensagem) {
+    if (!aluno) {
+        return mensagem;
+    }
+    
+    else return aluno;
 }
