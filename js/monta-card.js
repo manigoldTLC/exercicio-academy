e0cf1846edfb19dde5c4b7b4be43a4e7ceff2527
manigoldTLC <gabrxavier353@gmail.com>
2022-05-05 23:31:15 -0300

    agora o form aparece como um modal, alterado a forma como o conteudo é passado para o form

--- js/monta-card.js
+++ js/monta-card.js
@@ -1,5 +1,5 @@
 const listaAlunos = document.getElementById("lista-alunos");
-let card = ''
+let card = '';
 
 function getAlunos(alunos) {
     alunos.forEach(aluno => {
@@ -10,21 +10,16 @@ function getAlunos(alunos) {
                     <figure class="card__imgcontainer">
                         <img class="card__img" src="assets/img/manigold.png" alt="imagem usuário">
                     </figure>
-
-                    <div class="card__texts">
-                        <span class="card__name">${nullValue(aluno.nm_pessoa, "Nome não informado")}</span>
-                        <span class="card__birthdate">${nullValue(aluno.dt_nascimento, "Data de nascimento não informado")}</span>
-                    </div>
                 </div>
 
-                <div class="card__middle middle">
-                    <span class="card__obs-title">Observações</span>
-                    <span class="card__obs">${nullValue(aluno.ds_observacao, "Nenhuma observacao informada")}</span>
-                </div>
+                <span class="card__name">${nullValue(aluno.nm_pessoa, "Nome não informado")}</span>
+                <span class="card__birthdate">${nullValue(aluno.dt_nascimento, "Data de nascimento não informado")}</span>
 
-                <div class="card__bottom">
-                    <a class="card__a" href="edit-form.html">editar</a>
-                </div>
+                <span class="card__obs-title">Observações</span>
+                <span class="card__obs">${nullValue(aluno.ds_observacao, "Nenhuma observacao informada")}</span>
+
+                <button class="card__a" data-bs-toggle="modal" data-bs-target="#modal" id="editar">editar</button>
+
             </article>
         `;
     });
--- js/preenche-form.js
+++ js/preenche-form.js
@@ -1,45 +1,23 @@
-function editarAluno(id) {
-    const url = `https://five-turma-3.herokuapp.com/alunos/${id}`
+const alunos = document.getElementById("lista-alunos");
 
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            localStorage.setItem("aluno", JSON.stringify(data.aluno[0]));
-            listarAluno();
-        })
-}
+const nomeValue = document.querySelector("#nome");
+const nascimentoValue = document.querySelector("#nascimento");
+const observacaoValue = document.querySelector("#message-text");
 
+alunos.addEventListener("click", (e) => {
+    e.preventDefault();
 
-function listarAluno() {
-    window.location = "edit-form.html";
-}
+    let botaoEditar = e.target.id == "editar";
 
+    if (botaoEditar) {
+        const parent = e.target.parentNode;
 
-function preencheFormulario() {
-    const aluno = JSON.parse(localStorage.getItem("aluno"));
-    const nome = document.querySelector("#nome");
-    const nascimento = document.querySelector("#nascimento");
-    const cpf = document.querySelector("#cpf");
-    const cep = document.querySelector("#cep");
-    const endereco = document.querySelector("#endereco");
-    const numero = document.querySelector("#numero");
-    const complemento = document.querySelector("#complemento");
-    const bairro = document.querySelector("#bairro");
-    const cidade = document.querySelector("#cidade");
-    const estado = document.querySelector("#estado");
-    const telefone = document.querySelector("#telefone");
-    
-    nome.value = aluno.nm_pessoa;
-    nascimento.value = aluno.dt_nascimento;
-    cpf.value = aluno.ds_cpf;
-    cep.value = aluno.ds_cep;
-    endereco.value = aluno.ds_logradouro;
-    numero.value = aluno.ds_numero;
-    complemento.value = aluno.ds_complemento;
-    bairro.value = aluno.ds_bairro;
-    cidade.value = aluno.ds_cidade;
-    estado.value = aluno.ds_estado;
-    telefone.value = aluno.ds_telefone;
-}
+        let nomeContent = parent.querySelector(".card__name").textContent;
+        let nascimentoContent = parent.querySelector(".card__birthdate").textContent;
+        let observacaoContent = parent.querySelector(".card__obs").textContent;
 
-window.onload = preencheFormulario();
+        nomeValue.value = nomeContent;
+        nascimentoValue.value = nascimentoContent;
+        observacaoValue.value = observacaoContent;
+    }
+})
