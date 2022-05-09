93c5285d71ee91624adbd5e74ec1c675c6da47a2
manigoldTLC <gabrxavier353@gmail.com>
2022-05-08 22:41:19 -0300

    card agora com display grid e mostrando mais informações do aluno

--- js/monta-card.js
+++ js/monta-card.js
@@ -5,21 +5,24 @@ function getAlunos(alunos) {
     alunos.forEach(aluno => {
 
         card += `
-            <article class="card col-12 col-md-6 col-lg-4 col-xl-3">
-                <div class="card__top top">
-                    <figure class="card__imgcontainer">
-                        <img class="card__img" src="assets/img/manigold.png" alt="imagem usuário">
-                    </figure>
-                </div>
+            <article class="card col-xl-3 col-lg-4">
+                <img class="card__img" src="assets/img/manigold.png" alt="imagem usuário">
 
-                <div class="d-flex flex-column" data-id=${aluno.nr_sequencia}>
-                    <span class="card__name">${nullValue(aluno.nm_pessoa, "Nome não informado")}</span>
-                    <span class="card__birthdate">${nullValue(aluno.dt_nascimento, "Data de nascimento não informado")}</span>
+                <div class="card__conteudo" data-id=${aluno.nr_sequencia}>
+                    <span class="card__name">${nullValue(aluno.nm_pessoa, "não informado")}</span>
+                    <span class="card__birthdate">${tranformDate(aluno.dt_nascimento, "não informado")}</span>
+                
+                    <span class="card__title-dados">Dados do aluno</span>
 
-                    <span class="card__obs-title">Observações</span>
+                    <span class="card__left">${nullValue(aluno.ds_email, "não informado")}</span>
+                    <span class="card__right">${nullValue(aluno.ds_cpf, "não informado")}</span>
+                    <span class="card__left">${nullValue(aluno.ds_telefone, "não informado")}</span>
+                    <span class="card__right">${nullValue(aluno.ds_estado, "não informado")}</span>
+
+                    <span class="card__title-obs">Observações</span>
                     <span class="card__obs">${nullValue(aluno.ds_observacao, "Nenhuma observacao informada")}</span>
 
-                    <button class="card__a" data-bs-toggle="modal" data-bs-target="#modal" id="editar">editar</button>
+                    <button class="card__button" data-bs-toggle="modal" data-bs-target="#modal" id="editar">editar</button>
                 </div>
 
             </article>
@@ -35,4 +38,12 @@ function nullValue(aluno, mensagem) {
     }
     
     else return aluno;
+}
+
+function tranformDate(date, mensagem) {
+    if (!date) {
+        return mensagem;
+    }
+
+    return date.split("-").reverse().join("/")
 }
